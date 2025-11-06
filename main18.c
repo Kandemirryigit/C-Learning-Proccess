2a06e442a550390856f243f3d8dd9e6e3cc360b6
yiğit kandemir <kandemirryigit@gmail.com>
2025-11-06 19:57:29 +0300

    Add files via upload

---
+++ main18.c
@@ -0,0 +1,33 @@
+/*
+Find the average of 10 nuumbers
+*/
+
+
+#include <stdio.h>
+
+// %2u means show 2 digit before point
+
+int main()
+{
+    int grades[10];  // we just can store 10 numbers inside this array
+    int count=10;
+    float sum=0;
+    float average=0;
+
+    printf("Enter the ten numbers to be averaged\n");
+    for (int i=0;i<count;i++)
+    {
+        printf("%2u>",i+1);
+        scanf("%d",&grades[i]);
+        sum+=grades[i];
+    }
+
+    average=sum/count;
+    printf("\nAverage of the ten grades entered is: %.2f\n", average);
+
+}
+
+// Make sure that you dont cross the array bounds
+// Because if you do, you will get garbage values or your program may crash
+// And everything looks normal,The program dont give any error or warning
+// Because of this a man said "C is like a knife don't give it to the children"

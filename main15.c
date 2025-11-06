6ebdccece238c6bdb490c722b94f034d1a63ebd4
yiğit kandemir <kandemirryigit@gmail.com>
2025-11-06 17:59:59 +0300

    Add files via upload

---
+++ main15.c
@@ -0,0 +1,20 @@
+/*
+Learning while loop
+*/
+
+
+#include <stdio.h>
+
+
+// A while loop lets your program repeat something as long as a condition is true.
+// When the condition becomes false → the loop stops.
+
+int main()
+{
+    int i=0;
+    while (i<=10)
+    {
+        printf("\n%d",i);
+        i++ ; 
+    }
+}
